27c07b4f89b8017794dc90fed89d683cc7eddae8
UnesABA <unesaitbenali@gmail.com>
2025-02-17 16:38:26 +0100

    setting and styling country component

--- Travel-Journey/src/components/Country.jsx
+++ Travel-Journey/src/components/Country.jsx
@@ -1,20 +1,36 @@
 import React from 'react'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faParachuteBox } from "@fortawesome/free-solid-svg-icons";
 import Mount_fuji from '/Mount_fuji.jpg'
 
 const Country = () => {
   return (
-    <div className= "japan-container">
+    <div className= "country-container">
       <div>
         <img src= {Mount_fuji} className= "mount-fuji" alt="Mount Fuji" />
       </div>
       <div className= "about-mount-fuji">
-        <div>
+        <div className='location'>
           <div></div>
-          <div>JAPAN</div>
-          <div><u>View on Google Maps</u></div>
+          <p className= "country-name">JAPAN</p>
+          <u className= "google-maps-link">View on Google Maps</u>
         </div>
+
+        <div className= "place-name">
+          <h1>Mount Fuji</h1>
+        </div>
+        
+        <div>
+          <div className= "start-end-date">
+            <h3>12 jan, 2023</h3>
+            <h3>-</h3>
+            <h3>24 jan, 2023</h3>
+          </div>
+          <div className= "description">
+            <h4>
+              Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet), Mount Fuji is the single most popular tourist site in Japan, for both japanese and foreign tourists.
+            </h4>
+          </div>
+        </div>
+        
       </div>
     </div>
   )
