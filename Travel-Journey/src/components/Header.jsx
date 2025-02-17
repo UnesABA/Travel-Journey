5ca35db6e3750b77b0da838e35a77cf1195adccb
UnesABA <unesaitbenali@gmail.com>
2025-02-17 10:34:02 +0100

    Add header component with its logo

---
+++ Travel-Journey/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { FaGlobe } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <>
+      <div className= "header">
+        <div>
+          <FaGlobe className='header-icon'/>
+        </div>
+        <div className= "header-title">
+          my travel journey.
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header
